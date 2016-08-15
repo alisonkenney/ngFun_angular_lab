c6db7ec8df3b9b07e447ed6032a0bd71c64d082c
Alison Kenney <alisonkenney1@gmail.com>
2016-08-15 11:53:23 -0600

    finished lab

---
+++ app.js
@@ -0,0 +1,49 @@
+var app = angular.module("ngFun", []);
+
+app.controller('PokemonController', PokemonController);
+PokemonController.$inject = ['$scope'];
+
+function PokemonController($scope) {
+  $scope.catchphrase = "Gotta Catch Em All!";
+  $scope.trainer = {
+    name: "Ash"
+  };
+  $scope.pokemon = [
+    {
+      Ndex: 25,
+      name: 'Pikachu',
+      type: 'Electric'
+    },
+    {
+      Ndex: 10,
+      name: 'Caterpie',
+      type: 'Bug'
+    },
+    {
+      Ndex: 39,
+      name: 'Jigglypuff',
+      type: 'Fairy'
+    },
+    {
+      Ndex: 94,
+       name: 'Gengar',
+      type: 'Ghost'
+    },
+    {
+      Ndex: 143,
+      name: 'Snorlax',
+      type: 'Normal'
+    }
+  ];
+}
+
+app.filter('reverse', function() {
+  return function(input) {
+    input = input || '';
+    var out = input.split("").reverse().join("");
+    // uppercase only first letter
+    out = out.toLowerCase();
+    out = out[0].toUpperCase() + out.slice(1);
+    return out;
+  };
+});
